512166b5f73a2ed19d0c564fd1fb2012a9a5775c
IlchukV <110021062+IlchukV@users.noreply.github.com>
2023-04-19 19:52:41 +0000

    Deploying to gh-pages from @ IlchukV/goit-js-hw-09@fc38d944bc36defabddafca437cc97a43b99f93f 🚀

---
+++ 01-color-switcher.a3c81a7d.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),body:document.body};t.btnStart.addEventListener("click",(function(){n=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.btnStart.disabled=!0,t.btnStop.disabled=!1})),t.btnStop.addEventListener("click",(function(){clearInterval(n),t.btnStart.disabled=!1,t.btnStop.disabled=!0}));let n=null;
+//# sourceMappingURL=01-color-switcher.a3c81a7d.js.map
